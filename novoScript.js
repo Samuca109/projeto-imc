263d9da34675967ee0edcfc476b28e0d9c638d39
Samuel <samuelsantanadasilva8@gmail.com>
2022-12-24 18:16:38 -0300

    Atualizado

--- novoScript.js
+++ novoScript.js
@@ -1,51 +1,69 @@
-/* Variaveis do input*/
-let nome = document.querySelector('#nome').value
-let altura = document.querySelector('#altura').value
-let peso = document.querySelector('#peso').value
+/* letiaveis do input*/
+
 //Resposta
-let res = document.querySelector('#res')
-//Botao Calcular
-let botao = document.querySelector('#botao')
+let res = document.getElementById('res')
 //form
-let form = document.querySelector('#form')
+let form = document.getElementById('form')
+
 
 // Retorna uma arrow function
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+        e.preventDefault()
 
-    // Primeiro se faz uma validação e depois o calculo
-    if(nome !== '' && altura !== '' && peso !== ''){
-      
-        let calculoImc = (peso / (altura*altura)) //.toFixed(2)
-        let cliente = ''
+        // As variaveis tem que ficar dentro da funçao
+        let nome = document.getElementById('nome').value
+        //Forma diferente de localizar e poder adicionar mais funcionalidades do calculo e qualquer outra coisa
+        let altura = document.getElementById('altura').value 
+        let alturaCal = altura / 100
+        
+        let peso = document.getElementById('peso').value
+
+        // toFixed(2) quer dizer no maximo 2 casas decimais
+     
+        let calculoImc =   peso / (alturaCal*alturaCal).toFixed(2)
+
+
+         // Primeiro se faz uma validação e depois o calculo no caso o if
+        if(nome !== '' && altura !== '' && peso !== ''){
+            let cliente = '';
 
         if( calculoImc < 18.5){
             // abaixo do peso
-           cliente = 'Você Esta abaixo do peso'
-        } else if(calculoImc < 25){
+           cliente = 'Você esta abaixo do peso'
+        } else if(calculoIMC < 25){
             // peso ideal
-            cliente = 'Parabens você esta no peso ideal'
-        } else if(calculoImc < 30) {
+            cliente = 'Parabéns você esta no peso ideal'
+        } else if(calculoIMC < 30) {
             // levemente acima do peso
             cliente = 'Você está levemente acima do peso'
-        }  else if(calculoImc < 35){
+        }  else if(calculoIMC < 35){
               // 35 com obesidade grau 1
-              cliente = 'Você esta com obesidade grau 1'
-        } else if(calculoImc < 40){
+              cliente = 'Você está com obesidade grau 1'
+        } else if(calculoIMC < 40){
             // 40 com obesidade grau 2
-            cliente = 'Você esta com obesidade grau 2'
+            cliente = 'Você está com obesidade grau 2'
         }  else {
             // com obesidade grau 3 cuidado!!
             cliente = 'Cuide-se você esta com obesidade grau 3,procure se cuidar urgentemente'
-        }
+        } 
+
+        res.innerHTML = `<p>Olá ${nome} o seu calculo imc é ${calculoImc}. <br> ${cliente}</p>`
      } else {
-        alert('Preencha os valores corretamente')
-    }
+            alert('Preencha os campos corretamente')
+        }
+      
+    })
 
-    res.textContent = `<p>Ola ${nome} o seu calculo imc é ${calculoImc} e o seu resultado ${cliente}</p>`
-})
-
-console.log(res)
-
+    let limpar = document.querySelector('#limpar')
+    limpar.addEventListener('click', function(){
+        form.value = ''
+        res.remove()
+    })
+  
+    
+/*
+if(nome !== '' && altura !== '' && peso !== ''){
+} else {
+      alert('Preencha os campos corretamente')  */
 
 
